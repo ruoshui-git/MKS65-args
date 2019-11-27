127bd47109786f1ef6f86120d050a23d4c71d340
Ruoshui <rmao10@stuy.edu>
2019-11-27 06:31:13 -0500

    feat:done

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
+#define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 #include <unistd.h>
-#define DLT ' '
+#define DLT " "
 #define MAX_ARGS 5
 
 
@@ -10,15 +11,38 @@ char ** parse_args( char * line );
 
 int main(void)
 {
+    char * s = strdup("ls -al");
+    char ** args = parse_args(s);
+    int i = 0;
+    char * argv[MAX_ARGS + 1];
 
+    memcpy(argv, args, sizeof(char*) * MAX_ARGS + 1);
+    
+    for (i = 0; argv[i] != NULL; i++)
+    {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    free(args);
+    free(s);
+
+    execvp(argv[0], argv);
+
+    puts("You shouldn't see this!");
 }
 
 char ** parse_args( char * line )
 {
-    char ** args = malloc(sizeof(char *) * MAX_ARGS);
+    char ** args = malloc(sizeof(char *) * (MAX_ARGS + 1));
 
     int i = 0;
     char * curr = line;
     char * token;
-    token = strsep(line, DLT);
+    while (curr)
+    {
+        args[i] = strsep(&curr, DLT);
+        i++;
+    }
+    args[i] = NULL;
+    return args;
 }
